66f38e54c3c6fb7d9ee723e5cf39c6a8df93155d
Alyson Takai <shtakai@gmail.com>
2016-07-07 16:48:59 -0700

    fixed

--- app.js
+++ app.js
@@ -5,28 +5,8 @@ var static_contents = require('./lib/static_contents');
 
 // creating server
 server = http.createServer(function(request,response){
-  response.writeHead(200, {'Content-type': 'text/html'});
-  console.log(`Request ${request.url}`);
   static_contents(request, response);
 
-  //if(request.url === '/'){
-    //fs.readFile('views/index.html', 'utf8', function(error,contents){
-      //response.write(contents);
-      //response.end();
-    //});
-  //} else if(request.url === '/dojo.html'){
-    //fs.readFile('views/dojo.html', 'utf8', function(error, contents){
-      //response.write(contents);
-      //response.end();
-    //});
-  //} else if(request.url === '/stylesheet/style.css'){
-    //fs.readFile('stylesheet/style.css', 'utf8', function(error, contents){
-      //response.write(contents);
-      //response.end();
-    //});
-  //} else{
-    //response.end(`File not found. ${request.url}`);
-  //}
 });
 
 // listen server on port
